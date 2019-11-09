d99c75dfc8d0351b9029e6b53ae91ebf502aac03
Thore-Krug <36414231+Thore-Krug@users.noreply.github.com>
2019-11-09 23:48:13 +0100

    removed spinner & piping to /dev/null
    removed spinner and sending output to /dev/null to better debug

--- PocketDESK.sh
+++ PocketDESK.sh
@@ -21,45 +21,39 @@ echo "+---------------------------------------------------------------------+"
 
 
 
-# install dependencies
-start_spinner [Installing_dependencies]
-$MYDIR/Scripts/dependencies.sh &> /dev/null
-stop_spinner $?
+$MYDIR/Scripts/dependencies.sh 
 
-# Install PocketHome
-start_spinner [Installing_PocketHome]
-$MYDIR/Scripts/pockethome.sh &> /dev/null
-stop_spinner $?
 
-# Working Touchscreen
-start_spinner [Fixing_touchscreen]
-$MYDIR/Scripts/touchscreen.sh &> /dev/null
-stop_spinner $?
 
-# Working Keyboard
-start_spinner [Fixing_keyboard]
+
+$MYDIR/Scripts/pockethome.sh 
+
+
+
+$MYDIR/Scripts/touchscreen.sh 
+
+
+
 $MYDIR/Scripts/keyboard.sh &> /dev/null
-stop_spinner $?
 
-# Get a battery indicator
-start_spinner [Installing_battery_indicator]
-$MYDIR/Scripts/battery.sh &> /dev/null
-stop_spinner $?
 
-# Nicely named sessions
-start_spinner [Correcting_names]
-$MYDIR/Scripts/sessions.sh &> /dev/null
-stop_spinner $?
 
-# Setting Hostname
-start_spinner [Changing_Hostname]
-$MYDIR/Scripts/hostname.sh &> /dev/null
-stop_spinner $?
+$MYDIR/Scripts/battery.sh 
 
-# Wallpaper Change
-start_spinner [Selling_out]
-$MYDIR/Scripts/wallpaper.sh &> /dev/null
-stop_spinner $?
+
+
+$MYDIR/Scripts/sessions.sh
+
+
+
+
+$MYDIR/Scripts/hostname.sh 
+
+
+
+
+$MYDIR/Scripts/wallpaper.sh 
+
 
 
 # Finishing up
